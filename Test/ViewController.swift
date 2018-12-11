c6fca55d3164d420ec0c96d97ed15261b83b816e
Prabhu <prabhu@Prabhus-MacBook-Pro.local>
2018-12-11 12:30:22 +0530

    r2

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -12,18 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("4.1")
-        print("4.2")
-        print("4.3")
-        print("4.4")
-        print("4.5")
-        print("4.6")
+        print("r2")
     }
 
     func test() {
-        print("test 4.1")
-        print("test 4.2")
-        print("test 4.3")
+        
     }
 
 }
